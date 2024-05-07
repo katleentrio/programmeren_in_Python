f304d795facf00de113ff8c288af850a5199017e
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-05-07 12:36:58 +0200

    .

--- chapters/06 conditions/05 exercises/Zon/solution/solution.nl.py
+++
@@ -1,6 +0,0 @@
-weertype = input("Welk weer is het vandaag? Je kan kiezen tussen zon, regen of sneeuw: ")
-
-if weertype == "zon":
-    print("De zon schijnt vandaag. Neem je zonnebril mee.")
-
-print("Einde programma")
