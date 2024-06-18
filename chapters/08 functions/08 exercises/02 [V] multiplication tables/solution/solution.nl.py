3e90ec29d3eb5366835deffe0a12582270bf7b3c
katleentrio <161167371+katleentrio@users.noreply.github.com>
2024-06-18 11:56:31 +0200

    update oef

--- chapters/08 functions/08 exercises/02 [V] multiplication tables/solution/solution.nl.py
+++ chapters/08 functions/08 exercises/02 [V] multiplication tables/solution/solution.nl.py
@@ -1,4 +1,4 @@
 def tafel_van_vermenigvuldiging(getal):
 
     for factor in range(1, 11):
-        print(f'{factor} * {getal} = {factor * getal}')
+        return(f'{factor} * {getal} = {factor * getal}')
